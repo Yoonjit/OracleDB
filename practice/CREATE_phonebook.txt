81e1cf003cb87e99b59801a568d7bf0ae87f2376
Yoonjit <jisu9083@naver.com>
2023-04-09 14:07:59 +0900

    Test Again

--- practice/CREATE_phonebook.txt
+++
@@ -1,9 +0,0 @@
-CREATE TABLE phonebook (
-	id NUMBER PRIMARY KEY,
-	name VARCHAR2(50) NOT NULL,
-	phone VARCHAR2(100),
-	email VARCHAR2(100),
-	age NUMBER DEFAULT 21,
-	memo CLOB,
-	regDate DATE
-);
--- practice/create_professor_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE professor(
-			profno	NUMBER(4) PRIMARY KEY,
-			name	VARCHAR2(10) NOT NULL,
-			POSITION VARCHAR2(20) NOT NULL,
-			hiredate		DATE NOT NULL,
-			email		VARCHAR2(50)
-);
